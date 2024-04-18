1eb4f9800b60e97b2b24522b31286423d1eb1fb7
CaldeIsa <145611998+CaldeIsa@users.noreply.github.com>
2024-04-18 00:39:36 -0600

    Complete code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/main.scss';
-//import { initInputValidation, initSearchButton } from './js/form-validation';
+import { initInputValidation, initSearchButton } from './js/form-validation';
 
 (function init() {
   initInputValidation();
--- src/js/form-validation.js
+++ src/js/form-validation.js
@@ -15,37 +15,36 @@ function showResultsSection() {
 }
 
 function initInputValidation() {
+  var inputEmail = document.getElementById('email-search-input');
+  var inputPhone = document.getElementById('phone-search-input');
+  
   document.querySelectorAll('input[type="text"]').forEach(function (input) {
     input.addEventListener('keypress', function (event) {
-      const email = input.value.toLowerCase();
-      const regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-      if (email.match(regEx)) {
-        var x = true;
-        input.parentNode.classList.remove('error');
-      } else {
-        var x = false;
-      }
-      const keycode = event.keyCode ? event.keyCode : event.which;
-      if (keycode == '13') {
-        event.preventDefault();
-        localStorage.clear();
 
-        if (x === true) {
-          const proxyurl = '';
-          const url = 'https://ltvdataapi.devltv.co/api/v1/records?email=' + email;
-          fetch(proxyurl + url)
-            .then(function (response) {
-              return response.text();
-            })
-            .then(function (contents) {
-              localStorage.setItem('userObject', contents);
-              showResultsSection();
-            })
-            .catch(function (e) {
-              console.log(e);
-            });
-        } else if (x !== true) {
-          input.parentNode.classList.add('error');
+      if (inputEmail.style.display == 'block') {
+        const email = input.value.toLowerCase();
+        const regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        if (email.match(regEx)) {
+          input.parentNode.classList.remove('error-email');
+          document.querySelector('.error-msg').style.display = 'none';
+        } else {
+          input.parentNode.classList.add('error-email');
+          document.querySelector('.error-msg').style.display = 'block';
+
+        }
+      }
+      if(inputPhone.style.display=='block') {
+        
+        const phone = input.value;
+        const regEx = /^[0-9]{10}$/; 
+        if (phone.match(regEx)) {
+          input.parentNode.classList.remove('error-phone');
+          document.querySelector('.error-msg2').style.display = 'none';
+          
+        } else {
+          input.parentNode.classList.add('error-phone');
+          document.querySelector('.error-msg2').style.display = 'block';
+          console.log("entra");
         }
       }
     });
@@ -59,135 +58,88 @@ function initSearchButton() {
       localStorage.clear(); // Clears storage for next request
       const selector = e.currentTarget.dataset.form;
       const emailInput = document.getElementById(`email-${selector}-input`);
+      const phoneInput = document.getElementById(`phone-${selector}-input`);
       const email = emailInput.value.toLowerCase();
+      const phone = phoneInput.value;
 
-      let x;
-      const regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-      if (email.match(regEx)) {
-        x = true;
-      } else {
-        x = false;
+      if (emailInput.style.display == 'block') {
+        const regExEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        if (!email.match(regExEmail)) {
+          emailInput.parentNode.classList.add('error-email');
+          document.querySelector('.error-msg').style.display = 'block';
+          return;
+        }
       }
 
-      if (x === true) {
-        emailInput.parentNode.classList.remove('error');
-        const proxyurl = '';
-        const url = 'https://ltvdataapi.devltv.co/api/v1/records?email=' + email;
-        fetch(proxyurl + url)
-          .then(function (response) {
-            return response.text();
-          })
-          .then(function (contents) {
-            localStorage.setItem('userObject', contents);
-            showResultsSection();
-          })
-          .catch(function (e) {
-            console.log(e);
-          });
-      } else if (x !== true) {
-        emailInput.parentNode.classList.add('error');
+      if (phoneInput.style.display == 'block') {
+        const regExPhone = /^[0-9]{10}$/; 
+        if (!phone.match(regExPhone)) {
+          phoneInput.parentNode.classList.add('error-phone');
+          document.querySelector('.error-msg2').style.display = 'block';
+          return;
+        }
       }
+
+      const proxyurl = '';
+      let url;
+      if (inputEmail.style.display == 'block') {
+        url = 'https://ltvdataapi.devltv.co/api/v1/records?email=' + email;
+      } else if (inputPhone.style.display == 'block') {
+        url = 'https://ltvdataapi.devltv.co/api/v1/records?phone=' + phone;
+      }
+
+      fetch(proxyurl + url)
+        .then(function (response) {
+          return response.text();
+        })
+        .then(function (contents) {
+          localStorage.setItem('userObject', contents);
+          showResultsSection();
+        })
+        .catch(function (e) {
+          console.log(e);
+        });
     });
   });
 }
 
 
-
 document.getElementById('email-btn').addEventListener('click', function() {
-  document.getElementById('email-search-input').style.display = 'block';
-  document.getElementById('phone-search-input').style.display = 'none';
+  const emailInput = document.getElementById('email-search-input');
+  const phoneInput = document.getElementById('phone-search-input');
+
+  emailInput.style.display = 'block';
+  phoneInput.style.display = 'none';
+  emailInput.value = ''; 
+  phoneInput.value = ''; 
+  emailInput.parentNode.classList.remove('error-email');
+  phoneInput.parentNode.classList.remove('error-phone');
+  document.querySelector('.error-msg').style.display = 'none'; 
+  document.querySelector('.error-msg2').style.display = 'none'; 
   document.getElementById('email-btn').classList.remove('btn-secondary');
   document.getElementById('email-btn').classList.add('btn-primary');
   document.getElementById('phone-btn').classList.remove('btn-primary');
   document.getElementById('phone-btn').classList.add('btn-secondary');
+  document.getElementById('btn-go').style.display = 'block';
 });
 
 document.getElementById('phone-btn').addEventListener('click', function() {
-  document.getElementById('email-search-input').style.display = 'none';
-  document.getElementById('phone-search-input').style.display = 'block';
+  const emailInput = document.getElementById('email-search-input');
+  const phoneInput = document.getElementById('phone-search-input');
+
+  emailInput.style.display = 'none';
+  phoneInput.style.display = 'block';
+  emailInput.value = ''; 
+  phoneInput.value = ''; 
+  emailInput.parentNode.classList.remove('error-email');
+  phoneInput.parentNode.classList.remove('error-phone');
+  document.querySelector('.error-msg').style.display = 'none'; 
+  document.querySelector('.error-msg2').style.display = 'none'; 
   document.getElementById('email-btn').classList.remove('btn-primary');
   document.getElementById('email-btn').classList.add('btn-secondary');
   document.getElementById('phone-btn').classList.remove('btn-secondary');
   document.getElementById('phone-btn').classList.add('btn-primary');
+  document.getElementById('btn-go').style.display = 'block';
 });
-
-function initInputValidation2() {
-  document.querySelectorAll('input[type="number"]').forEach(function (input) {
-    input.addEventListener('keypress', function (event) {
-      
-      const phone = input.value;
-      const regExDigits = /^\d{10}$/;
-
-      if (phone.match(regExDigits)) {
-        var x = true;
-        input.parentNode.classList.remove('error2');
-      } else {
-        var x = false;
-      }
-      const keycode = event.keyCode ? event.keyCode : event.which;
-      if (keycode == '13') {
-        event.preventDefault();
-        localStorage.clear();
-
-        if (x === true) {
-          const proxyurl = '';
-          const url = 'https://ltvdataapi.devltv.co/api/v1/records?=phone' + phone ;
-          fetch(proxyurl + url)
-            .then(function (response) {
-              return response.text();
-            })
-            .then(function (contents) {
-              localStorage.setItem('userObject', contents);
-              showResultsSection();
-            })
-            .catch(function (e) {
-              console.log(e);
-            });
-        } else if (x !== true) {
-          input.parentNode.classList.add('error2');
-        }
-      }
-    });
-  });
-}
-
-function initPhoneSearchButton() {
-  document.querySelectorAll('.js-btn-search-phone').forEach(function (button) {
-    button.addEventListener('click', function (e) {
-      e.preventDefault();
-      localStorage.clear(); // Clears storage for next request
-      const selector = e.currentTarget.dataset.form;
-      const phoneInput = document.getElementById(`phone-${selector}-input`);
-      const phone = phoneInput.value;
-
-      let x;
-      const regExDigits = /^\d{10}$/;
-      if (phone.match(regExDigits)) {
-        x = true;
-      } else {
-        x = false;
-      }
-
-      if (x === true) {
-        phoneInput.parentNode.classList.remove('error2');
-        const proxyurl = '';
-        const url = 'https://ltvdataapi.devltv.co/api/v1/records?phone=' + phone;
-        fetch(proxyurl + url)
-          .then(function (response) {
-            return response.text();
-          })
-          .then(function (contents) {
-            localStorage.setItem('userObject', contents);
-            showResultsSection();
-          })
-          .catch(function (e) {
-            console.log(e);
-          });
-      } else if (x !== true) {
-        phoneInput.parentNode.classList.add('error2');
-      }
-    });
-  });
-}
 
 export { initInputValidation, initSearchButton };
